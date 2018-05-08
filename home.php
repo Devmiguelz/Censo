ca888540c82879532d6a21d19c7a107e7e41caac
Devmiguelz <zmiguel96@gmail.com>
2018-05-08 13:06:10 -0500

    Initial commit

---
+++ home.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<meta name="description" content="">
+  	<meta name="author" content="">
+	<title>Bienvenido al Censo Poblacional 2018</title>
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<link rel="stylesheet" href="css/custom.css">
+  	
+</head>
+<body>
+	<div class="col-md-12 fondo">
+		<h1 class="">Bienvenido(a) al Censo Poblacional 2018</h1>
+		<p class="titulo">Es necesario que complete <strong>todas las preguntas de cada sección para poder avanzar a la siguiente.</strong></p>
+		<div class="salir"><a><strong>SALIR</strong></a></div>
+		<div class="form-group row">
+			<div class="col-md-3">
+				<div class="seccion11" id="seccion11">
+					<p class="font-weight-bold text-center texto" id="texto1" for="seccion">UBICACIÓN</p>
+					<img  class="imag" src="images/ubicacion.png" alt="">
+				</div>
+				<div class=" btn seccion1">
+					<a class="star" id="empezar1" href="#"><strong>EMPEZAR<span class="glyphicon glyphicon-chevron-right"></span></strong></a>
+				</div>
+				<p>Preguntas sobre la ubicación de su vivienda.</p>
+			</div>
+			<div class="col-md-3">
+				<div class="seccion22" id="seccion22">
+					<p  class="font-weight-bold text-center texto" id="texto2" for="seccion">VIVIENDA</p>
+					<img  class="imag" src="images/vivienda.png" alt="">
+				</div>
+				<div class=" btn seccion2">
+					<a class="star" id="empezar2" href="#"><strong>EMPEZAR<span class="glyphicon glyphicon-chevron-right"></span></strong></a>
+				</div>
+				<p>Preguntas sobre la vivienda en la reside habitualmente.</p>
+			</div>
+			<div class="col-md-3">
+				<div class="seccion33" id="seccion33">
+					<p class="font-weight-bold text-center texto" id="texto3" for="seccion">HOGAR</p>
+					<img  class="imag" src="images/hogar.png" alt="">
+				</div>
+				<div class=" btn seccion3">
+					<a class="star" id="empezar3" href="#"><strong>EMPEZAR<span class="glyphicon glyphicon-chevron-right"></span></strong></a>
+				</div>
+				<p>Preguntas sobre las caracteristicas de su hogar.</p>
+			</div>
+			<div class="col-md-3">
+				<div class="seccion44" id="seccion44">
+					<p class="font-weight-bold text-center texto" id="texto4" for="seccion">PERSONAS</p>
+					<img  class="imag" src="images/personas.png" alt="">
+				</div>
+				<div class=" btn seccion4">
+					<a class="star"  id="empezar4" href="#"><strong>EMPEZAR<span class="glyphicon glyphicon-chevron-right"></span></strong></a>
+				</div>
+				<p>Preguntas sobre las personas que conforman su hogar.</p>
+			</div>
+		</div>
+	</div>
+	<div class="text-center progress_bar">
+         <div class="progress">
+            <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 10%;" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">10%</div>
+        </div>
+    </div>	
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="js/myscript.js" type="text/javascript" charset="utf-8"></script>
+</body>
+</html>
